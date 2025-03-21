2b9f15d72fe1a8c14f74618ae6a005f05657d80e
rossvc <117058340+rossvc@users.noreply.github.com>
2025-03-21 17:00:27 -0500

    cleanup nav bar

--- views/Homepage.js
+++ views/Homepage.js
@@ -6,8 +6,4 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("logInBtn").addEventListener("click", function () {
     window.location.href = "Signin.html";
   });
-
-  document.getElementById("menuIcon").addEventListener("click", function () {
-    alert("Menu clicked! You can implement functionality here.");
-  });
 });
